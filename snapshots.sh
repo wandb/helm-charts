3f0776f4a09fe553fa7a3458c344d88dadd7e4f6
Daniel Barnes <dabarnes2b@gmail.com>
2025-07-03 14:35:32 +0900

    Revert "slow down"
    This reverts commit eda8f8a63e5e4f1a37ee7b7da2d7b19698e98318.

--- snapshots.sh
+++ snapshots.sh
@@ -19,7 +19,6 @@ function main() {
       echo "Checking snapshot tests"
       for value_file in ./$values_dir/$chart/*; do
         if [[ -f "$value_file" ]]; then
-          sleep 1
           helm chartsnap -c "./charts/$chart" -f "$value_file"
         fi
       done
