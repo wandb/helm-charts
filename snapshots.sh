3247f39326a550bf4396ec8ae33996bd618b4e8c
Daniel Barnes <dabarnes2b@gmail.com>
2025-09-24 07:29:21 +0900

    update snapshots / script

--- snapshots.sh
+++ snapshots.sh
@@ -20,7 +20,7 @@ function usage() {
   cat <<EOF
 A helper script for the helm-chartsnap plugin https://github.com/jlandowner/helm-chartsnap
 
-Usage: $0 [COMMAND]
+Usage: $0 [COMMAND] [optional: CHART (ex: operator-wandb)]
 
 Commands:
   build,    build the operator-wandb chart
@@ -29,7 +29,28 @@ Commands:
 EOF
 }
 
+function build_chart() {
+  local chart="$1"
+  echo "Building $chart"
+  helm cascade build "./charts/$chart"
+}
+
+function update_chart() {
+  local chart="$1"
+  local values_file="$2"
+  echo "updating $chart snapshots"
+  helm chartsnap -c "./charts/$chart" -u -f "$values_file"
+}
+
+function run_chart() {
+  local chart="$1"
+  local values_file="$2"
+  echo "Checking $chart snapshot tests"
+  helm chartsnap -c "./charts/$chart" -f "$values_file"
+}
+
 function main() {
+  local func="$1"
   local charts=("wandb-base" "operator-wandb")
   local values_dir="test-configs"
 
@@ -38,21 +59,20 @@ function main() {
     exit 1
   fi
 
-  local func="$1"
+  if [ -n "$2"  ]; then
+    charts=("$2")
+  fi
 
   for chart in "${charts[@]}"; do
     case "$func" in
       build)
-        echo "Building $chart"
-        helm cascade build "./charts/$chart"
+        build_chart "$chart"
         ;;
       update)
-        echo "Updating $chart snapshots"
-        helm chartsnap -c "./charts/$chart" -u -f "./$values_dir/$chart"
+        update_chart "$chart" "./$values_dir/$chart"
         ;;
       run)
-        echo "Checking $chart snapshot tests"
-        helm chartsnap -c "./charts/$chart" -f "./$values_dir/$chart"
+        run_chart "$chart" "./$values_dir/$chart"
         ;;
       *)
         usage
